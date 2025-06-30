f27a57bfdca5eb6c18e65fb885c39749ff98fc69
john-hession <97198860+john-hession@users.noreply.github.com>
2025-06-30 18:30:42 -0400

    Finish toy problem #1

--- components/layers.py
+++ components/layers.py
@@ -3,7 +3,7 @@ import numpy as np
 #https://medium.com/@z.shaked/building-micrograd-with-vectorization-a-step-by-step-guide-dcf897fc54cb
 class Linear:
     def __init__(self, in_size, out_size):
-        self.weights = np.random.rand(in_size, out_size)
+        self.weights = np.random.normal(0, np.sqrt(2/in_size), (in_size, out_size))
         self.biases = np.zeros((1,out_size))
         self.x = None
         self.output = None
@@ -25,13 +25,14 @@ class Linear:
             raise ValueError("Forward pass must be called before backward pass.")
         
         # Calculate gradients
+       
         weights_gradient = np.dot(self.x.T, out_grad)
         bias_gradient = np.sum(out_grad, axis=0, keepdims=True)
         input_gradient = np.dot(out_grad, self.weights.T)
         
         # Update weights and biases
         self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * bias_gradient
+        self.biases -= learning_rate * bias_gradient
         
         return input_gradient
     
